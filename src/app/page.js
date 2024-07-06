13f7982ba88f070695c97c062a0a195ba371bf36
joelwalice <joelwalice@gmail.com>
2024-07-06 19:17:30 +0530

    FEAT: Updated Pages

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center">
-      <Link href="/login"><p>Inventory Management</p></Link>
-    </main>
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-5">
+      <h1 className="flex justify-center text-xl md:text-3xl text-green-600 font-semibold">Inventory Management</h1>
+      <button className="rounded-lg bg-red-500 text-white p-2 border-0 shadow-lg">
+        <Link href="/login" className="flex items-center justify-center text-2xl ">
+          Login
+        </Link>
+      </button>
+    </div>
   );
 }
